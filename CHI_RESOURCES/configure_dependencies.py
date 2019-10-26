2e52fd44890f58c4cb7b9eefb7acbb0113eac387
Jan <janicvermaak@gmail.com>
2019-10-26 13:18:57 -0500

    Second rendition of build system

--- CHI_RESOURCES/configure_dependencies.py
+++ CHI_RESOURCES/configure_dependencies.py
@@ -1,11 +1,7 @@
 import sys
-import time
 import os
 import subprocess
-import multiprocessing
-
-boost_url = "https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.gz"
-petsc_url = "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.9.4.tar.gz"
+import errno
 
 print("########## Chi-Tech Dependency installer ##########")
 
@@ -32,6 +28,7 @@ if (not os.path.exists(install_dir)):
   os.mkdir("chi-dependencies")
 
 log_file = open(install_dir + "/log.txt","w+")
+roots_file = open(install_dir + "/configure_deproots.sh","w+")
 
 #######################################
 # Prints a value to cout using system
@@ -68,7 +65,7 @@ def ExecSub(command,log,env_vars=None):
 def CheckForCCompilers():
   result = True
   print("Looking for c compiler...",end="")
-  success,err = ExecSub("gcc --version",log_file)
+  success,err = ExecSub("gcxc --version",log_file)
 
   if (success):
     print("Success")
@@ -118,7 +115,223 @@ def CheckDependencyDir():
   else:
     os.chdir(install_dir)
 
+####################################### Install Eigen
+eigen_url = "http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz"
+def InstallEigen():
+  if (not os.path.exists(install_dir + "/EIGEN")):
+    os.mkdir("EIGEN")
+  os.chdir("EIGEN")
+
+  if (not os.path.exists(install_dir + "/EIGEN/3.3.7.tar.gz")):
+    print("Downloading Eigen 3.3.7 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + eigen_url,log_file)
+
+  #Check if eigen is installed already
+  installed = os.path.exists(install_dir + "/EIGEN/eigen-3.3.7/Eigen")
+  if (not installed):
+    print("Configuring Eigen 3.3.7 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf 3.3.7.tar.gz",log_file)
+    success,err = ExecSub("mv eigen-* eigen-3.3.7",log_file)
+  else:
+    print("EIGEN already installed")
+
+  os.chdir(install_dir)
+
+####################################### Install Random-123
+r123_url = "https://www.deshawresearch.com/downloads/download_random123.cgi/Random123-1.13.2.tar.gz"
+def InstallRandom123():
+  if (not os.path.exists(install_dir + "/RANDOM123")):
+    os.mkdir("RANDOM123")
+  os.chdir("RANDOM123")
+
+  if (not os.path.exists(install_dir + "/RANDOM123/Random123-1.13.2.tar.gz")):
+    print("Downloading Random123-1.13.2 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + r123_url,log_file)
+
+  #Check if it is installed already
+  installed = os.path.exists(install_dir + "/RANDOM123/Random123-1.13.2/include")
+  if (not installed):
+    print("Configuring Random123-1.13.2 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf Random123-1.13.2.tar.gz",log_file)
+  else:
+    print("RANDOM123 already installed")
+
+  os.chdir(install_dir)
+
+####################################### Install triangle
+tri_url = "http://www.netlib.org/voronoi/triangle.zip"
+def InstallTriangle():
+  if (not os.path.exists(install_dir + "/TRIANGLE")):
+    os.mkdir("TRIANGLE")
+  os.chdir("TRIANGLE")
+
+  if (not os.path.exists(install_dir + "/TRIANGLE/triangle.zip")):
+    print("Downloading Triangle to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + tri_url,log_file)
+
+  #Check if it is installed already
+  installed = os.path.exists(install_dir + "/TRIANGLE/triangle.o")
+  if (not installed):
+    print("Configuring Triangle to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("unzip triangle.zip",log_file)
+
+    env_vars=os.environ.copy()
+    command = "gcc -O -DAPPLE -I/usr/X11R6/include" + \
+              " -L/usr/X11R6/lib -o ./triangle " + \
+              " ./triangle.c -lm"
+    success,err = ExecSub(command,log_file,env_vars)
+    print(command,err)
+
+    command = "gcc -O -DAPPLE -I/usr/X11R6/include " + \
+              "-L/usr/X11R6/lib -DTRILIBRARY -c -o " + \
+              "./triangle.o ./triangle.c"
+    success,err = ExecSub(command,log_file,env_vars)
+    print(command,err)
+  else:
+    print("Triangle already installed")
+
+  os.chdir(install_dir)
+
+####################################### Install readline
+readline_url = "ftp://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz"
+def InstallReadline():
+  if (not os.path.exists(install_dir + "/READLINE")):
+    os.mkdir("READLINE")
+  os.chdir("READLINE")
+
+  if (not os.path.exists(install_dir + "/READLINE/readline-8.0.tar.gz")):
+    print("Downloading Readline 8.0 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + readline_url,log_file)
+
+  #Check if it is installed already
+  installed = os.path.exists(install_dir + "/READLINE/readline-8.0"
+                                           "/build/lib/libreadline.a")
+  if (not installed):
+    print("Configuring Readline 8.0 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf readline-8.0.tar.gz",log_file)
+
+    os.chdir("readline-8.0")
+    env_vars=os.environ.copy()
+    command = "./configure --prefix=" + \
+              install_dir + \
+              "/READLINE/readline-8.0/build"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+
+    command = "make -j8"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+
+    command = "make install"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+  else:
+    print("Readline already installed")
+
+  os.chdir(install_dir)
+
+####################################### Install ncurses
+ncurses_url = "https://invisible-mirror.net/archives/ncurses/ncurses-6.1.tar.gz"
+lua_url = "https://www.lua.org/ftp/lua-5.3.5.tar.gz"
+def Install_ncurses():
+  if (not os.path.exists(install_dir + "/NCURSES")):
+    os.mkdir("NCURSES")
+  os.chdir("NCURSES")
+
+  if (not os.path.exists(install_dir + "/NCURSES/ncurses-6.1.tar.gz")):
+    print("Downloading NCurses 6.1 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + ncurses_url,log_file)
+
+  #Check if it is installed already
+  installed = os.path.exists(install_dir + "/NCURSES/ncurses-6.1"
+                                           "/build/lib/libncurses.a")
+  if (not installed):
+    print("Configuring ncurses 6.1 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf ncurses-6.1.tar.gz",log_file)
+
+    os.chdir("ncurses-6.1")
+    env_vars=os.environ.copy()
+    command = "./configure --prefix=" + \
+              install_dir + \
+              "/NCURSES/ncurses-6.1/build"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+
+    command = "make -j8"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+
+    command = "make install"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+  else:
+    print("Ncurses already installed")
+
+  os.chdir(install_dir)
+
+####################################### Install lua
+lua_url = "https://www.lua.org/ftp/lua-5.3.5.tar.gz"
+def InstallLua():
+  if (not os.path.exists(install_dir + "/LUA")):
+    os.mkdir("LUA")
+  os.chdir("LUA")
+
+  if (not os.path.exists(install_dir + "/LUA/lua-5.3.5.tar.gz")):
+    print("Downloading Lua 5.3.5 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + lua_url,log_file)
+
+  #Check if it is installed already
+  installed = os.path.exists(install_dir + "/LUA/lua-5.3.5"
+                                           "/install/lib/liblua.a")
+  if (not installed):
+    print("Configuring LUA 5.3.5 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf lua-5.3.5.tar.gz",log_file)
+
+    os.chdir("lua-5.3.5")
+    env_vars=os.environ.copy()
+
+    lib_path = ""
+    if ("LIBRARY_PATH" in env_vars):
+      lib_path = env_vars["LIBRARY_PATH"]
+    lib_path = lib_path + ":" + install_dir + "/READLINE/readline-8.0/build/lib"
+    lib_path = lib_path + ":" + install_dir + "/READLINE/ncurses-6.1/build/lib"
+    env_vars["LIBRARY_PATH"] = lib_path
+
+    c_path = ""
+    if ("CPATH" in env_vars):
+      c_path = env_vars["CPATH"]
+    c_path = env_vars["CPATH"]
+    c_path = c_path + ":" + install_dir + "/READLINE/readline-8.0/build/include"
+    env_vars["CPATH"] = c_path
+
+    os_tag = "linux"
+    if ("Darwin" in os.uname() ):
+      os_tag = "macosx"
+
+    command = "make " + os_tag + " MYLIBS=-lncurses -j8"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+    print(command,err)
+
+    command = "make local"
+    success,err = ExecSub(command,log_file,env_vars)
+    if (not success):
+      print(command,err)
+  else:
+    print("Lua already installed")
+
+  os.chdir(install_dir)
+
+
 ####################################### Install boost
+boost_url = "https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.gz"
 def InstallBoost():
   if (not os.path.exists(install_dir + "/BOOST")):
     os.mkdir("BOOST")
@@ -146,6 +359,7 @@ def InstallBoost():
   os.chdir(install_dir)
 
 ####################################### Install PETSc
+petsc_url = "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.9.4.tar.gz"
 def InstallPETSc():
   if (not os.path.exists(install_dir + "/PETSc")):
     os.mkdir("PETSc")
@@ -178,7 +392,6 @@ def InstallPETSc():
         "--download-parmetis=1 " \
         "--download-superlu_dist=1 " \
         "--with-cxx-dialect=C++11 " \
-        "--download-mpich=1 "\
         "CFLAGS='-fPIC -fopenmp' " \
         "CXXFLAGS='-fPIC -fopenmp' " \
         "FFLAGS='-fPIC -fopenmp' " \
@@ -192,28 +405,97 @@ def InstallPETSc():
 
     print(exstring)
     success,err = ExecSub(exstring,log_file,env_vars)
+    success,err = ExecSub("make all",log_file,env_vars)
+    success,err = ExecSub("make install",log_file,env_vars)
   else:
     print("PETSc already installed")
 
-CheckForCCompilers()
-CheckForCPPCompilers()
-CheckFortranCompilers()
+  os.chdir(install_dir)
+
+
+####################################### Install VTK
+vtk_url   = "https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz"
+def InstallVTK():
+  if (not os.path.exists(install_dir + "/VTK")):
+    os.mkdir("VTK")
+  os.chdir("VTK")
+
+  if (not os.path.exists(install_dir + "/VTK/VTK-8.2.0.tar.gz")):
+    print("Downloading VTK 8.2.0 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("wget " + vtk_url,log_file)
+
+
+  # Check if vtk is installed already
+  installed = os.path.exists(install_dir + "/VTK/VTK-8.2.0/install/lib")
+  if (not installed):
+    print("Configuring VTK 8.2.0 to \"" + os.getcwd() + "\"")
+    success,err = ExecSub("tar -zxf VTK-8.2.0.tar.gz",log_file)
+    os.chdir("VTK-8.2.0/")
+    os.mkdir("build")
+    os.chdir("build")
+
+    env_vars=os.environ.copy()
+    success,err = ExecSub("cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install " + \
+                          "-DBUILD_SHARED_LIBS:BOOL=ON " + \
+                          "-DVTK_Group_MPI:BOOL=ON " + \
+                          "-DVTK_GROUP_ENABLE_Qt=NO " + \
+                          "-DVTK_GROUP_ENABLE_Rendering=NO " + \
+                          "-DVTK_GROUP_ENABLE_Imaging=NO " + \
+                          "-DVTK_GROUP_ENABLE_StandAlone=WANT " + \
+                          "-DVTK_GROUP_ENABLE_Web=NO " + \
+                          "-DVTK_BUILD_TESTING:BOOL=OFF " + \
+                          "-DCMAKE_BUILD_TYPE=Release " + \
+                          "-DCMAKE_CXX_FLAGS=-std=c++11 " + \
+                          " ../",log_file,env_vars)
+    success,err = ExecSub("make -j8",log_file,env_vars)
+    success,err = ExecSub("make install",log_file,env_vars)
+  else:
+    print("VTK already installed")
+
+
+success = CheckForCCompilers()
+if (not success):
+  print("***** c compiler not working")
+  exit(errno.EPERM)
+success = CheckForCPPCompilers()
+if (not success):
+  print("***** c++ compiler not working")
+  exit(errno.EPERM)
+success = CheckFortranCompilers()
+if (not success):
+  print("***** Fortran compiler not working")
+  exit(errno.EPERM)
+
+
+
 CheckDependencyDir()
+InstallEigen()
+InstallRandom123()
+InstallTriangle()
+InstallReadline()
+Install_ncurses()
+InstallLua()
+
 InstallBoost()
 InstallPETSc()
+InstallVTK()
+
+
+
+roots_file.write('export BASE_PATH="' + install_dir + '"\n')
+roots_file.write('\n')
+roots_file.write('export EIGEN_ROOT="$BASE_PATH/EIGEN/Eigen-3.3.7"\n')
+roots_file.write('export RANDOM123_ROOT="$BASE_PATH/RANDOM123/Random123-1.13.2"\n')
+roots_file.write('export TRIANGLE_ROOT="/$BASE_PATH/Triangle"\n')
+roots_file.write('export LUA_ROOT="$BASE_PATH/LUA/lua-5.3.5/install"\n')
+roots_file.write('export BOOST_ROOT="$BASE_PATH/BOOST/boost_1_71_0"\n')
+roots_file.write('export PETSC_ROOT="$BASE_PATH/PETSc/petsc-3.9.4/install"\n')
+roots_file.write('export VTK_DIR="$BASE_PATH/VTK/VTK-8.2.0/install"\n')
+
+ExecSub("chmod u+x configure_deproots.sh",log_file)
 
 log_file.close()
+roots_file.close()
 
-########## FOR DEMO ################
-if __name__ == "__main__":
-  print("Mother..0%")
-  for i in range(1,101):
-      time.sleep(0.01)
-      sprint_esc("[1A")
-      sprint_esc("[2K")
-      print("Mother.." + str(i) + "%")
-
-
-
-####################################
-
+print("########## Chi-Tech Dependency install complete ##########")
+print("     Now execute: \". ./chi-dependencies/configure_deproots.sh\"")
