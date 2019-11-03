e189652149ce47ac196b2929031607ebe9622594
Jan <janicvermaak@gmail.com>
2019-11-03 11:17:51 -0600

    Revert "Added multiple math utilities for matrix and vector ops"
    This reverts commit 600eff14d292fb12f179bc288cae88cef82629c4.

---
+++ CHI_TECH/ChiMath/chi_math_solvers.cpp
@@ -0,0 +1,48 @@
+#include "chi_math.h"
+#include <assert.h>
+
+//############################################################################# GAUSS ELIMINATION
+/** Gauss Elimination without pivoting.*/
+void ChiMath::GaussElimination(std::vector<std::vector<double> > &A,
+	                              std::vector<double> &b, int n)
+{
+	// Forward elimination
+	for(int i = 0;i < n-1;++i)
+	{
+		const std::vector<double>& ai = A[i];
+		double bi = b[i];
+		double factor = 1.0/A[i][i];
+		for(int j = i+1;j < n;++j)
+		{
+			std::vector<double>& aj = A[j];
+			double val = aj[i] * factor;
+			b[j] -= val * bi;
+			for(int k = i+1;k < n;++k)
+				aj[k] -= val * ai[k];
+		}
+	}
+
+	// Back substitution
+	for(int i = n-1;i >= 0;--i)
+	{
+		const std::vector<double>& ai = A[i];
+		double bi = b[i];
+		for(int j = i+1;j < n;++j)
+			bi -= ai[j] * b[j];
+		b[i] = bi/ai[i];
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
