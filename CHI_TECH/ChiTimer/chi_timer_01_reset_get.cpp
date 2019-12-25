1563ca167b94f324acd3fbb9588e904b652b4884
Jan <janicvermaak@gmail.com>
2019-12-24 18:06:10 -0600

    Fixed printout of weird characters when requesting date-time string

--- CHI_TECH/ChiTimer/chi_timer_01_reset_get.cpp
+++ CHI_TECH/ChiTimer/chi_timer_01_reset_get.cpp
@@ -49,8 +49,10 @@ std::string ChiTimer::GetLocalDateTimeString()
   using namespace std::chrono;
   std::time_t now = system_clock::to_time_t(system_clock::now());
 
-  std::string s(30, '\0');
-  std::strftime(&s[0], s.size(), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+  char s[30];
+  int end = std::strftime(s, 30, "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+  s[29] = '\0';
+  if (end < 30)  s[end]='\0';
   return s;
 }
 
