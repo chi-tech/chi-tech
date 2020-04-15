58b4127b8db15b0536dd88ff9e822af5ece0659b
Jan <janicvermaak@gmail.com>
2020-04-15 11:32:17 -0500

    Update to documentation

--- CHI_DOC/PAGES/ProgrammersManual/ProgrammersManual.h
+++ CHI_DOC/PAGES/ProgrammersManual/ProgrammersManual.h
@@ -1,11 +1,8 @@
 /**\page ProgrammerManual Developer's manual
  *
- *\subpage DevManDevCycle \n
- *\subpage DevManAddingCode \n
- *\subpage DevManCodeConventions \n
- *\subpage DevManGlobalVars \n
- *\subpage DevManAddingLua \n
+ *\subpage DevManMustReads \n
  *\subpage DevManMeshes \n
+ *\subpage DevManDevCycle \n
  *
 
 
