6b2f51b1e539e9bde3cca7b35c4aeba94ca5cde5
Jan <janicvermaak@gmail.com>
2020-09-10 15:57:43 -0500

    Random number generator

---
+++ CHI_TECH/ChiMath/RandomNumberGeneration/random_number_generator.h
@@ -0,0 +1,23 @@
+#ifndef _chi_math_rng_h
+#define _chi_math_rng_h
+
+#include <random>
+
+namespace chi_math
+{
+//#########################################################
+/**Random number generator based on threefry.*/
+class RandomNumberGenerator
+{
+private:
+  std::mt19937_64    mt1993764_generator;
+  std::uniform_real_distribution<double> distribution;
+
+public:
+  RandomNumberGenerator();
+  RandomNumberGenerator(int seed);
+  double Rand();
+};
+}
+
+#endif
