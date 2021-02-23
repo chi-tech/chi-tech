51fa5cfe5052ef78baeb2960123683377932d833
Jan <janicvermaak@gmail.com>
2021-02-23 17:04:35 -0600

    Significantly improved unpartitioned volume mesher

--- CHI_TECH/ChiMesh/VolumeMesher/PredefinedUnpartitioned/volmesher_predefunpart_connectivity.cc
+++ CHI_TECH/ChiMesh/VolumeMesher/PredefinedUnpartitioned/volmesher_predefunpart_connectivity.cc
@@ -11,77 +11,74 @@ void chi_mesh::VolumeMesherPredefinedUnpartitioned::
 {
   //======================================== Populate vertex
   //                                                   subscriptionns
-  std::vector<std::set<int>> vertex_subs(umesh->vertices.size());
-  int c=-1;
-  for (auto cell : umesh->raw_cells)
+  std::vector<std::set<size_t>> vertex_subs(umesh->vertices.size());
+  uint64_t cur_cell_id=0;
+  for (auto& cell : umesh->raw_cells)
   {
-    ++c;
     for (auto vid : cell->vertex_ids)
-      vertex_subs[vid].insert(c);
+      vertex_subs[vid].insert(cur_cell_id);
+    ++cur_cell_id;
   }
 
   int num_bndry_faces = 0;
   for (auto cell : umesh->raw_cells)
     for (auto& face : cell->faces)
+    {
       if (face.neighbor < 0) ++num_bndry_faces;
+      face.neighbor = -1;
+    }
 
-  chi_log.Log(LOG_0) << "Number of bndry faces: " << num_bndry_faces;
+  chi_log.Log(LOG_0VERBOSE_1) << "Number of boundary faces "
+                                 "before connectivity: " << num_bndry_faces;
 
   //======================================== Establish connectivity
-  std::vector<std::set<int>> cells_to_search;
-  c=-1;
-  for (auto cell : umesh->raw_cells)
+  chi_log.Log() << "Establishing cell connectivity.";
+  std::set<size_t> cells_to_search;
+  cur_cell_id=0;
+  for (auto& cell : umesh->raw_cells)
   {
-    ++c;
-    for (auto& face : cell->faces)
+    cells_to_search.clear();
+    for (uint64_t vid : cell->vertex_ids)
+      for (uint64_t cell_id : vertex_subs[vid])
+        cells_to_search.insert(cell_id);
+
+    for (auto& cur_cell_face : cell->faces)
     {
-      if (face.neighbor >= 0) continue;
+      if (cur_cell_face.neighbor >= 0 ) continue;
+      std::set<uint64_t> cfvids(cur_cell_face.vertex_ids.begin(),
+                                cur_cell_face.vertex_ids.end());
 
-      cells_to_search.clear();
-      for (auto cfvid : face.vertex_ids)
-        cells_to_search.push_back(vertex_subs[cfvid]);
-
-      std::set<int> cfvids(face.vertex_ids.begin(),
-                           face.vertex_ids.end());
-
-      bool stop_searching = false;
-      for (auto& adj_cell_id_set : cells_to_search)
+      for (uint64_t adj_cell_id : cells_to_search)
       {
-        for (auto& adj_cell_id : adj_cell_id_set)
+        auto adj_cell = umesh->raw_cells[adj_cell_id];
+
+        for (auto& adj_cell_face : adj_cell->faces)
         {
-          if (adj_cell_id == c) continue;
-          auto adj_cell = umesh->raw_cells[adj_cell_id];
+          if (adj_cell_face.neighbor >= 0) continue;
+          std::set<uint64_t> afvids(cur_cell_face.vertex_ids.begin(),
+                                    cur_cell_face.vertex_ids.end());
 
-          bool adj_cell_matches = false;
-          for (auto& aface : adj_cell->faces)
+          if (cfvids == afvids)
           {
-            std::set<int> afvids(aface.vertex_ids.begin(),
-                                 aface.vertex_ids.end());
-
-            if (afvids == cfvids)
-            {
-              adj_cell_matches = true;
-              break;
-            }
+            cur_cell_face.neighbor = adj_cell_id;
+            adj_cell_face.neighbor = cur_cell_id;
+            goto cell_neighbor_found;
           }
-
-          if (adj_cell_matches)
-          {
-            face.neighbor = adj_cell_id;
-            stop_searching = true;
-          }
-
-          if (stop_searching) break;
-        }//cell id
-        if (stop_searching) break;
-      }//cell id set
+        }//for adjacent cell face
+      }
     }//for face
+
+    cell_neighbor_found:
+    ++cur_cell_id;
   }//for cell
+
+  chi_log.Log() << "Done establishing cell connectivity.";
 
   num_bndry_faces = 0;
   for (auto cell : umesh->raw_cells)
     for (auto& face : cell->faces)
       if (face.neighbor < 0) ++num_bndry_faces;
 
-  chi_log.Log(LOG_0) << "Number of bndry faces: " << num_bndry_faces;
+  chi_log.Log(LOG_0VERBOSE_1) << "Number of boundary faces "
+                                 "after connectivity: " << num_bndry_faces;
 }
