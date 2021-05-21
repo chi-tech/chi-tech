20ca0df1ef83273cb48c675cca3966fcad43d746
Dominic Caron <dominic.caron@tamu.edu>
2021-05-21 13:04:49 -0500

    pwl cell base - add documentation

--- ChiTech/ChiMath/SpatialDiscretization/CellMappings/FE_PWL/pwl_cellbase.h
+++ ChiTech/ChiMath/SpatialDiscretization/CellMappings/FE_PWL/pwl_cellbase.h
@@ -10,34 +10,44 @@
 class CellMappingFE_PWL
 {
 protected:
+  /** Mesh. */
   chi_mesh::MeshContinuumPtr grid;
 public:
-  const int num_nodes;
-
   typedef std::vector<double> VecDbl;
   typedef std::vector<chi_mesh::Vector3> VecVec3;
-
+  /** Number of nodes of the cell. */
+  const int num_nodes;
+  /** For each cell face, map from the face node index to the corresponding
+   *  cell node index. More specifically, \p face_dof_mappings[f][fi], with
+   *  \p fi the face node index of the face identified by face index \p f,
+   *  contains the corresponding cell node index. */
   std::vector<std::vector<int>> face_dof_mappings;
 
 public:
+  /** Constructor. */
   explicit CellMappingFE_PWL(int num_dofs,
                              chi_mesh::MeshContinuumPtr ref_grid) :
     grid(std::move(ref_grid)),
     num_nodes(num_dofs)
   {}
 
+  /** Compute unit integrals. */
   virtual void
   ComputeUnitIntegrals(chi_math::finite_element::UnitIntegralData& ui_data);
 
+  /** Initialize volume quadrature point data and
+   *  surface quadrature point data for all faces. */
   virtual void
   InitializeAllQuadraturePointData(
     chi_math::finite_element::InternalQuadraturePointData& internal_data,
     std::vector<chi_math::finite_element::FaceQuadraturePointData>& faces_qp_data) {}
 
+  /** Initialize volume quadrature point data. */
   virtual void
   InitializeVolumeQuadraturePointData(
     chi_math::finite_element::InternalQuadraturePointData& internal_data) {}
 
+  /** Initialize surface quadrature point data for face index \p face. */
   virtual void
   InitializeFaceQuadraturePointData(
     unsigned int face,
@@ -71,6 +81,8 @@ public:
   {
     gradshape_values.resize(num_nodes, chi_mesh::Vector3());
   }
+
+  /** Destructor. */
   virtual ~CellMappingFE_PWL() = default;
 };
 
