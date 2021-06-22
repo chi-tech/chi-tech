b525c49b57803b174c9a2a1432b46001c589a735
Zachary Hardy <zhardy1115@gmail.com>
2021-06-22 18:18:16 -0500

    Modified SetSource for additional fission flags.

--- ChiModules/LinearBoltzmannSolver/IterativeOperations/lbs_setsource.cc
+++ ChiModules/LinearBoltzmannSolver/IterativeOperations/lbs_setsource.cc
@@ -29,9 +29,10 @@ void LinearBoltzmann::Solver::
   const bool apply_mat_src         = (source_flags & APPLY_MATERIAL_SOURCE);
   const bool apply_wgs_scatter_src = (source_flags & APPLY_WGS_SCATTER_SOURCE);
   const bool apply_ags_scatter_src = (source_flags & APPLY_AGS_SCATTER_SOURCE);
-  const bool apply_fission_src     = (source_flags & APPLY_WGS_FISSION_SOURCE);
+  const bool apply_wgs_fission_src = (source_flags & APPLY_WGS_FISSION_SOURCE);
+  const bool apply_ags_fission_src = (source_flags & APPLY_AGS_FISSION_SOURCE);
 
-  //================================================== Get group setup
+  //============================== Get group setup
   int gs_i = groupset.groups[0].id;
   int gs_f = groupset.groups.back().id;
 
@@ -42,14 +43,13 @@ void LinearBoltzmann::Solver::
 
   std::vector<double> default_zero_src(groups.size(),0.0);
 
-  //================================================== Loop over local cells
+  //======================================== Loop over local cells
   for (const auto& cell : grid->local_cells)
   {
     auto& full_cell_view = cell_transport_views[cell.local_id];
 
-    //=========================================== Obtain cross-section and src
+    //==================== Obtain cross-section and src
     int cell_matid = cell.material_id;
-
     int xs_id = matid_to_xs_map[cell_matid];
     int src_id= matid_to_src_map[cell_matid];
 
@@ -62,89 +62,108 @@ void LinearBoltzmann::Solver::
 
     auto xs = material_xs[xs_id];
 
-    //=========================================== Obtain material source
+    //==================== Obtain material source
     double* src = default_zero_src.data();
     if ( (src_id >= 0) && (apply_mat_src) )
       src = material_srcs[src_id]->source_value_g.data();
 
-    //=========================================== Loop over dofs
+    //============================== Loop over nodes
     int num_nodes = full_cell_view.NumNodes();
-    for (int i=0; i <num_nodes; i++)
+    for (int i = 0; i < num_nodes; ++i)
     {
-      for (int m=0; m<num_moments; ++m)
+      //============================== Loop over moments
+      for (int m = 0; m < num_moments; ++m)
       {
         unsigned int ell = m_to_ell_em_map[m].ell;
 
-        size_t ir      = full_cell_view.MapDOF(i,m,0);
+        size_t ir        = full_cell_view.MapDOF(i,m,0);
         double* q_mom    = &destination_q[ir];
         double* phi_oldp = &phi_old_local[ir];
 
-        //============================= Loop over groupset groups
-        for (int g=gs_i; g<=gs_f; g++)
+        //============================== Loop over groupset groups
+        for (int g = gs_i; g <= gs_f; ++g)
         {
-          if ( apply_mat_src && (ell == 0) )
+          //======================================== Apply material source
+          if (apply_mat_src && (ell == 0))
             q_mom[g] += src[g];
 
-          double inscat_g = 0.0;
-          //====================== Apply across-groupset scattering
-          if ((ell < xs->transfer_matrices.size()) && (apply_ags_scatter_src) )
+
+          //======================================== Apply scattering
+          if (ell < xs->transfer_matrices.size())
           {
-            size_t num_transfers = xs->transfer_matrices[ell].rowI_indices[g].size();
-            for (int t=0; t<num_transfers; t++)
+            double inscat_g = 0.0;
+            //======================================== Across-groupset
+            if (apply_ags_scatter_src)
             {
-              size_t gprime = xs->transfer_matrices[ell].rowI_indices[g][t];
-              if ((gprime < gs_i) || (gprime > gs_f))
+              size_t num_transfers =
+                  xs->transfer_matrices[ell].rowI_indices[g].size();
+
+              //============================== Loop over transfers
+              for (int t = 0; t < num_transfers; ++t)
               {
-                double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
-                inscat_g += sigma_sm * phi_oldp[gprime];
+                size_t gprime = xs->transfer_matrices[ell].rowI_indices[g][t];
+                if ((gprime < gs_i) or (gprime > gs_f))
+                {
+                  double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
+                  inscat_g += sigma_sm * phi_oldp[gprime];
+                }
               }
             }
-          }//if moment avail
 
-          //====================== Apply within-groupset scattering
-          if ((ell < xs->transfer_matrices.size()) && (apply_wgs_scatter_src) )
-          {
-            size_t num_transfers = xs->transfer_matrices[ell].rowI_indices[g].size();
-            for (int t=0; t<num_transfers; t++)
+            //======================================== Within-groupset
+            if (apply_wgs_scatter_src)
             {
-              size_t gprime = xs->transfer_matrices[ell].rowI_indices[g][t];
-              if ((gprime >= gs_i) && (gprime<=gs_f))
+              size_t num_transfers =
+                  xs->transfer_matrices[ell].rowI_indices[g].size();
+
+              //============================== Loop over transfers
+              for (int t = 0; t < num_transfers; ++t)
               {
-                double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
-                inscat_g += sigma_sm * phi_oldp[gprime];
+                size_t gprime = xs->transfer_matrices[ell].rowI_indices[g][t];
+                if ((gprime >= gs_i) and (gprime <= gs_f))
+                {
+                  double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
+                  inscat_g += sigma_sm * phi_oldp[gprime];
+                }
               }
             }
+            q_mom[g] += inscat_g;
           }//if moment avail
 
-          q_mom[g] += inscat_g;
 
-          //====================== Apply accross-groupset fission
-          if ( (ell == 0) and (apply_fission_src) )
+          //======================================== Apply fission
+          if ((xs->is_fissile) and (ell == 0))
           {
-            for (size_t gprime=first_grp; gprime<=last_grp; ++gprime)
+            double infission_g = 0.0;
+            //======================================== Across-groupset
+            if (apply_ags_fission_src)
             {
-              if ((gprime < gs_i) || (gprime > gs_f))
+              //================================ Loop over groups
+              for (size_t gprime = first_grp; gprime <= last_grp; ++gprime)
               {
-                q_mom[g] += xs->chi[g] *
-                            xs->nu_sigma_f[gprime] *
-                            phi_oldp[gprime];
-              }
-            }//for gprime
-          }//if zeroth moment
+                if ((gprime < gs_i) or (gprime > gs_f))
+                  infission_g += xs->chi[g] *
+                                 xs->nu_sigma_f[gprime] *
+                                 phi_oldp[gprime];
+              }//for gprime
+            }//if zeroth moment
 
-          //====================== Apply within-groupset fission
-          if ( (ell == 0) and (apply_fission_src) )
-          {
-            for (size_t gprime=first_grp; gprime<=last_grp; ++gprime)
+            //======================================== Within-groupset
+            if (apply_wgs_fission_src)
             {
-              if ((gprime >= gs_i) && (gprime<=gs_f))
+              //============================== Loop over groups
+              for (size_t gprime = first_grp; gprime <= last_grp; ++gprime)
               {
-                q_mom[g] += xs->chi[g] *
-                            xs->nu_sigma_f[gprime] *
-                            phi_oldp[gprime];
-              }
-            }//for gprime
-          }//if zeroth moment
+                if ((gprime >= gs_i) and (gprime <= gs_f))
+                  infission_g += xs->chi[g] *
+                                 xs->nu_sigma_f[gprime] *
+                                 phi_oldp[gprime];
+              }//for gprime
+            }//if zeroth moment
+
+            q_mom[g] += infission_g;
+          }//if fissile and ell == 0
+
         }//for g
       }//for m
     }//for dof i
