0c9da96268905b73cb94b820f28a15a640928f83
Zachary Hardy <zhardy1115@gmail.com>
2021-06-22 20:47:42 -0500

    Refactor header file.

--- ChiModules/KEigenvalueSolver/k_eigenvalue_solver.h
+++ ChiModules/KEigenvalueSolver/k_eigenvalue_solver.h
@@ -6,7 +6,7 @@
 
 #include <string>
 
-namespace  LinearBoltzmann { namespace KEigenvalue
+namespace  LinearBoltzmann::KEigenvalue
 {
 
 /**A k-eigenvalue neutron transport solver.*/
@@ -16,35 +16,21 @@ private:
   size_t source_event_tag;
 
 public:
-
-  // Total number of precursors on all materials
-  size_t num_precursors;
-
-  // Current estimate of the k-eigenvalue
   double k_eff = 1.0;
 
-  /** This is a local-to-global index mapping for the precursors
-  //  defined on each material. The precursor vector Nj_new_local
-  //  stores all precursor concentrations defined in the problem
-  //  on each node, whether a precursor exists on the material a
-  //  node is located on. This necessitates a global numbering
-  //  system for the precursors. The TransportCrossSections
-  // class defines all precursor information locally, with indices
-  // from 0 to num_precursors - 1 for that material. The precursor_map
-  // is a vector of vectors containing the global numbering of the
-  // precursor species. The outer vector is of the same length as
-  // the number of material_xs vector. Each of the inner vectors have
-  // length corresponding to num_precursors for the corresponding
-  // TransportCrossSection object.
-  **/
-  std::vector<std::vector<size_t>> precursor_map;
+  size_t num_precursors;
 
-  // Additional phi vector
   std::vector<double> phi_prev_local;
 
-  // Precursor vector and unknown manager
   std::vector<double> Nj_new_local;
   chi_math::UnknownManager Nj_unk_man;
+
+  // This structure maps local precursor indices to
+  // global precursor indices. This is used to assign
+  // a global numbering system for precursors which
+  // becomes important when multiple materials with
+  // precursors exist within a problem.
+  std::vector<std::vector<size_t>> precursor_map;
 
   // IterativeMethods
   void PowerIteration();
@@ -59,9 +45,8 @@ public:
   // Execute method
   void InitializeKSolver();
   void ExecuteKSolver();
-  
 };
 
-} }
+}
 
 #endif
