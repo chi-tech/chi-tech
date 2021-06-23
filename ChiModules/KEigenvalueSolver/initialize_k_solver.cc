c99a571136ec536dd4bb59440d6295543604b0be
Zachary Hardy <zhardy1115@gmail.com>
2021-06-22 20:56:14 -0500

    Cleanup initialize.

--- ChiModules/KEigenvalueSolver/initialize_k_solver.cc
+++ ChiModules/KEigenvalueSolver/initialize_k_solver.cc
@@ -10,48 +10,40 @@ using namespace LinearBoltzmann;
 //###################################################################
 void KEigenvalue::Solver::InitializeKSolver()
 {
-  // ----- General LBS init
+  //============================== Parent class initialize
   Initialize();
 
-  // ----- Init additional phi vector
+  //============================== Initialize phi vector
   phi_prev_local.resize(phi_old_local.size(),0.0);
 
-  // ----- Init precursors
+  //============================== Initialize precursors
   if (options.use_precursors) {
     num_precursors = 0;
-
-    // ----- Count precursors and define mapping
-    /** This computes the total number of precursors
-     // in the problem across all materials and assigns
-     //  a global mapping to the material. For example,
-     //  if material 0 has 6 precursors and material 1
-     //  has 6, the global mapping will give material 0
-     //  precursor IDs 0-5 and material 1 will receive
-     //  precursor IDs 6-11.
-    **/
     precursor_map.clear();
 
-    // Loop over materials
+    //============================== Loop over materials
     for (auto& xs : material_xs) {
-      // Material precursor mapping vector
-      std::vector<size_t> mat_map;
+      //============================== Setup map for material
+      std::vector<size_t> mat_map(xs->num_precursors);
 
-      // Define the precursor mapping for this material
+      //============================== Loop over precursors
       for (size_t j = 0; j < xs->num_precursors; ++j)
-         mat_map.emplace_back(num_precursors + j);
+        mat_map.emplace_back(num_precursors + j);
 
-      // Increment the total number of precursors
+      //============================== Increment precursors
       num_precursors += xs->num_precursors;
 
-      // Add mapping to the precursor map
+      //============================== Add mapping to structure
       precursor_map.emplace_back(mat_map);
     }
 
-    // -----Initialize precursor unknown manager and vector
+
     if (num_precursors > 0) {
+      //============================== Initialize precursor unknown manager
       auto pwl = std::static_pointer_cast<SpatialDiscretization_PWLD>(discretization);
       Nj_unk_man.AddUnknown(chi_math::UnknownType::VECTOR_N, num_precursors);
 
+      //============================== Initialize precursor vector
       int local_Nj_dof_count = pwl->GetNumLocalDOFs(Nj_unk_man);
       Nj_new_local.resize(local_Nj_dof_count, 0.0);
     }
