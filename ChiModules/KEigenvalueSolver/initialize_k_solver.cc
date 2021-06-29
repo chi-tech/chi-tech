12e14ae180987bd9f71260a78d585ff4ded6b88c
Zachary Hardy <44588104+zachhardy@users.noreply.github.com>
2021-06-29 14:28:56 -0500

    Cleanup.

--- ChiModules/KEigenvalueSolver/initialize_k_solver.cc
+++ ChiModules/KEigenvalueSolver/initialize_k_solver.cc
@@ -11,13 +11,11 @@ using namespace LinearBoltzmann;
 //###################################################################
 void KEigenvalue::Solver::InitializeKSolver()
 {
-  //============================== Parent class initialize
-  Initialize();
+  Initialize(); // LinearBoltzmann::Solver initialize
 
-  //============================== Initialize phi vector
   phi_prev_local.resize(phi_old_local.size(), 0.0);
 
-  //============================== Init precursors
+  //======================================== Initialize precursors
   if (options.use_precursors)
   {
     //clear precursor properties
@@ -32,8 +30,7 @@ void KEigenvalue::Solver::InitializeKSolver()
         max_num_precursors_per_material = xs->num_precursors;
     }
 
-    //============================== Initialize precursor vector and
-    //                               unknown manager
+
     if (num_precursors > 0)
     {
       typedef SpatialDiscretization_PWLD  PWLD;
@@ -42,8 +39,8 @@ void KEigenvalue::Solver::InitializeKSolver()
       precursor_uk_man.AddUnknown(chi_math::UnknownType::VECTOR_N,
                                   max_num_precursors_per_material);
 
-      int local_Nj_dof_count = pwl->GetNumLocalDOFs(precursor_uk_man);
-      precursor_new_local.resize(local_Nj_dof_count, 0.0);
+      int local_precursor_dof_count = pwl->GetNumLocalDOFs(precursor_uk_man);
+      precursor_new_local.resize(local_precursor_dof_count, 0.0);
     }
-  }
+  }//if use precursors
 }
