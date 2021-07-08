b8135339c68d218571108acd013a55655fc07399
Zachary Hardy <44588104+zachhardy@users.noreply.github.com>
2021-07-08 16:55:43 -0600

    Removed temporary memory pointers.

--- ChiModules/LinearBoltzmannSolver/IterativeOperations/lbs_setsource.cc
+++ ChiModules/LinearBoltzmannSolver/IterativeOperations/lbs_setsource.cc
@@ -32,7 +32,7 @@ SetSource(LBSGroupset& groupset,
   const bool apply_wgs_fission_src = (source_flags & APPLY_WGS_FISSION_SOURCE);
   const bool apply_ags_fission_src = (source_flags & APPLY_AGS_FISSION_SOURCE);
 
-  //======================================== Get group setup
+  //================================================== Get group setup
   auto gs_i = static_cast<size_t>(groupset.groups[0].id);
   auto gs_f = static_cast<size_t>(groupset.groups.back().id);
 
@@ -44,12 +44,12 @@ SetSource(LBSGroupset& groupset,
 
   std::vector<double> default_zero_src(groups.size(), 0.0);
 
-  //======================================== Loop over local cells
+  //================================================== Loop over local cells
   for (const auto& cell : grid->local_cells)
   {
     auto& full_cell_view = cell_transport_views[cell.local_id];
 
-    //==================== Obtain xs and src
+    //==================== Obtain xs
     int cell_matid = cell.material_id;
     int xs_id = matid_to_xs_map[cell_matid];
     int src_id = matid_to_src_map[cell_matid];
@@ -63,35 +63,34 @@ SetSource(LBSGroupset& groupset,
 
     auto xs = material_xs[xs_id];
 
-    //======================================== Apply material source
+    //==================== Obtain src
     double* src = default_zero_src.data();
     if ((src_id >= 0) && (apply_mat_src))
       src = material_srcs[src_id]->source_value_g.data();
 
-    //============================== Loop over nodes
-    int num_nodes = full_cell_view.NumNodes();
+    //======================================== Loop over nodes
+    const int num_nodes = full_cell_view.NumNodes();
     for (int i = 0; i < num_nodes; ++i)
     {
-      //============================== Loop over moments
+      //=================================== Loop over moments
       for (int m = 0; m < num_moments; ++m)
       {
         unsigned int ell = m_to_ell_em_map[m].ell;
 
         size_t ir = full_cell_view.MapDOF(i, m, 0);
-        double* q_mom = &destination_q[ir];
-        double* phi_oldp = &phi_old_local[ir];
 
-        //============================== Loop over groupset groups
+
+        //=================================== Loop over groupset groups
         for (size_t g = gs_i; g <= gs_f; ++g)
         {
-          //======================================== Apply material source
+          //=================================== Apply material source
           if (apply_mat_src && (ell == 0))
-            q_mom[g] += src[g];
+            destination_q[ir + g] += src[g];
 
           double inscatter_g = 0.0;
           const bool moment_avail = (ell < xs->transfer_matrices.size());
 
-          //============================== Apply across-groupset scattering
+          //=================================== Apply across-groupset scattering
           if (moment_avail and apply_ags_scatter_src)
           {
             size_t num_transfers =
@@ -106,12 +105,12 @@ SetSource(LBSGroupset& groupset,
               if ((gprime < gs_i) or (gprime > gs_f))
               {
                 double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
-                inscatter_g += sigma_sm * phi_oldp[gprime];
+                inscatter_g += sigma_sm * phi_old_local[ir + gprime];
               }
             }
           }//if moment_avail
 
-          //============================== Apply within-groupset scattering
+          //=================================== Apply within-groupset scattering
           if (moment_avail and apply_wgs_scatter_src)
           {
             size_t num_transfers =
@@ -124,17 +123,17 @@ SetSource(LBSGroupset& groupset,
               if ((gprime >= gs_i) and (gprime <= gs_f))
               {
                 double sigma_sm = xs->transfer_matrices[ell].rowI_values[g][t];
-                inscatter_g += sigma_sm * phi_oldp[gprime];
+                inscatter_g += sigma_sm * phi_old_local[ir + gprime];
               }
             }
           }
-          q_mom[g] += inscatter_g;
+          destination_q[ir + g] += inscatter_g;
 
 
           double infission_g = 0.0;
           const bool fission_avail = ((xs->is_fissile) and (ell == 0));
 
-          //============================== Apply accross-groupset fission
+          //=================================== Apply accross-groupset fission
           if (fission_avail and apply_ags_fission_src)
           {
             //================================ Loop over groups
@@ -143,24 +142,23 @@ SetSource(LBSGroupset& groupset,
               if ((gprime < gs_i) or (gprime > gs_f))
                 infission_g += xs->chi[g] *
                                xs->nu_sigma_f[gprime] *
-                               phi_oldp[gprime];
+                               phi_old_local[ir + gprime];
             }//for gprime
           }//if zeroth moment
 
-          //============================== Apply within-groupset fission
+          //=================================== Apply within-groupset fission
           if (fission_avail and apply_wgs_fission_src)
           {
-            //================================ Loop over groups
+            //============================== Loop over groups
             for (size_t gprime = first_grp; gprime <= last_grp; ++gprime)
             {
               if ((gprime >= gs_i) or (gprime <= gs_f))
                 infission_g += xs->chi[g] *
                                xs->nu_sigma_f[gprime] *
-                               phi_oldp[gprime];
+                               phi_old_local[ir + gprime];
             }
           }
-
-          q_mom[g] += infission_g;
+          destination_q[ir + g] += infission_g;
 
         }//for g
       }//for m
