baf4b22ea5070fecc5ca19402c397e5c640644b2
Jan <janicvermaak@gmail.com>
2021-07-15 17:28:47 -0500

    Forgot LuaCheckIntegerValue in header

--- ChiTech/ChiLua/chi_lua.h
+++ ChiTech/ChiLua/chi_lua.h
@@ -17,6 +17,7 @@ void LuaCheckNilValue(const std::string& func_name, lua_State* L, int arg);
 void LuaCheckStringValue(const std::string& func_name, lua_State* L, int arg);
 void LuaCheckBoolValue(const std::string& func_name, lua_State* L, int arg);
 void LuaCheckNumberValue(const std::string& func_name, lua_State* L, int arg);
+void LuaCheckIntegerValue(const std::string& func_name, lua_State* L, int arg);
 void LuaCheckTableValue(const std::string& func_name, lua_State* L, int arg);
 std::string LuaSourceInfo(lua_State* L, const char* func_name);
 void LuaPopulateVectorFrom1DArray(const std::string& func_name,
