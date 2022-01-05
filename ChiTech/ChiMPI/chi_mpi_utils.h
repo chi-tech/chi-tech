4390a659740aa5c7d641310ed9c64602041bf19c
Jan <janicvermaak@gmail.com>
2022-01-05 12:25:00 -0600

    More face based neighbor errors fixed

---
+++ ChiTech/ChiMPI/chi_mpi_utils.h
@@ -0,0 +1,11 @@
+#ifndef CHITECH_CHI_MPI_UTILS_H
+#define CHITECH_CHI_MPI_UTILS_H
+
+#include "chi_mpi_utils_map_all2all.h"
+
+namespace chi_mpi_utils
+{
+
+}//namespace chi_mpi_utils
+
+#endif //CHITECH_CHI_MPI_UTILS_H
