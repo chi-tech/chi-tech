3a33cfd4b62f223a978517d37f53d6a6fe8e87d7
Jean Ragusa <jean.ragusa@tamu.edu>
2022-12-03 10:55:02 -0600

    adding lua registration

---
+++ ChiModules/DiffusionSolvers/lua/ds_lua_utils.h
@@ -0,0 +1,33 @@
+#ifndef CFEM_DIFFUSION_LUA_UTILS_H
+#define CFEM_DIFFUSION_LUA_UTILS_H
+
+#include"ChiLua/chi_lua.h"
+
+int chiCFEMDiffusionSolverCreate(lua_State *L);
+int chiCFEMDiffusionSetBCProperty(lua_State *L);
+
+#include "../cfem_diffusion_solver.h"
+
+namespace cfem_diffusion
+{
+  namespace cfem_diffusion_lua_utils
+  {
+    //###################################################################
+    /** Obtains a pointer to a cfem_diffusion::Solver object or an object
+     * derived from chi_physics::Solver
+     *
+     * \param handle int Index in the chi_physics_handler where the solve object
+     *                   should be located.
+     * \param calling_function_name string The string used to print error messages,
+     *                              should uniquely identify the calling function.
+     *
+     */
+    cfem_diffusion::Solver& GetSolverByHandle(
+      int handle, const std::string& calling_function_name);
+
+    void RegisterLuaEntities(lua_State *L);
+  }//namespace cfem_diffusion_lua_utils
+}//namespace cfem_diffusion
+
+
+#endif //CFEM_DIFFUSION_LUA_UTILS_H
