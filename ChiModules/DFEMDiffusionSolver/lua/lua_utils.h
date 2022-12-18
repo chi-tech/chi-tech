ab63ee6bdb419b7b75b8e7d4ac91c483cab7d9cb
Jean Ragusa <jean.ragusa@tamu.edu>
2022-12-18 14:21:25 -0600

    update to lua_utils.h in dfem solver

---
+++ ChiModules/DFEMDiffusionSolver/lua/lua_utils.h
@@ -0,0 +1,19 @@
+#ifndef DFEM_DIFFUSION_LUA_UTILS_H
+#define DFEM_DIFFUSION_LUA_UTILS_H
+
+#include"ChiLua/chi_lua.h"
+// #include "../dfem_diffusion_solver.h"
+
+int chiDFEMDiffusionSolverCreate(lua_State *L);
+int chiDFEMDiffusionSetBCProperty(lua_State *L);
+
+
+namespace dfem_diffusion
+{
+  namespace dfem_diffusion_lua_utils
+  {
+    void RegisterLuaEntities(lua_State *L);
+  }//namespace dfem_diffusion_lua_utils
+}//namespace dfem_diffusion
+
+#endif //DFEM_DIFFUSION_LUA_UTILS_H
