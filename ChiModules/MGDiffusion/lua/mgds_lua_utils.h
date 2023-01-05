3c0bff2a8e648a44d8248b2b3330630ff9b97a31
Jean Ragusa <jean.ragusa@tamu.edu>
2023-01-04 22:24:58 -0600

    rebasing-2

---
+++ ChiModules/MGDiffusion/lua/mgds_lua_utils.h
@@ -0,0 +1,16 @@
+#ifndef CFEM_MG_DIFFUSION_LUA_UTILS_H
+#define CFEM_MG_DIFFUSION_LUA_UTILS_H
+
+#include"ChiLua/chi_lua.h"
+#include "../mg_diffusion_solver.h"
+
+namespace mg_diffusion::mgd_lua_utils
+{
+  int chiCFEMMGDiffusionSolverCreate(lua_State *L);
+  int chiCFEMMGDiffusionSetBCProperty(lua_State *L);
+
+  void RegisterLuaEntities(lua_State *L);
+}//namespace mg_diffusion
+
+
+#endif //CFEM_MG_DIFFUSION_LUA_UTILS_H
