df65051b916d31142e626dbaa6d5df06685ec50a
Zachary Hardy <zhardy1115@gmail.com>
2023-01-05 15:52:35 -0700

    cleanup xs class

--- ChiTech/ChiPhysics/PhysicsMaterial/transportxsections/material_property_transportxsections.h
+++ ChiTech/ChiPhysics/PhysicsMaterial/transportxsections/material_property_transportxsections.h
@@ -17,40 +17,45 @@ namespace chi_physics
 class TransportCrossSections : public chi_physics::MaterialProperty
 {
 public:
-  size_t num_groups=0;                          ///< Total number of Groups
-  size_t scattering_order=0;                    ///< Legendre scattering order
-  size_t num_precursors=0;                      ///< Number of precursors
-  bool is_fissile = false;                      ///< Fissile or not
+  unsigned int num_groups=0;       ///< Total number of Groups
+  unsigned int scattering_order=0; ///< Legendre scattering order
+  unsigned int num_precursors=0;   ///< Number of precursors
 
-  typedef double GrpVal;                        ///< Denoting value per group
-  typedef double PrecursorVal;                  ///< Denoting value per precursor
+  bool is_fissile = false;
 
-  std::vector<GrpVal> sigma_t;                  ///< Total cross section
-  std::vector<GrpVal> sigma_f;                  ///< Sigmaf cross section
-  std::vector<GrpVal> sigma_a;                  ///< Pure absorption
-  std::vector<GrpVal> chi;                      ///< Fission spectrum
-  std::vector<GrpVal> chi_prompt;               ///< Prompt fission spectrum
-  std::vector<GrpVal> nu;                       ///< Nubar
-  std::vector<GrpVal> nu_prompt;                ///< Nubar-prompt
-  std::vector<GrpVal> nu_delayed;               ///< Nubar-delayed
-  std::vector<GrpVal> nu_sigma_f;               ///< Nubar-Sigmaf cross section
-  std::vector<GrpVal> nu_prompt_sigma_f;        ///< Prompt-Nubar-Sigmaf cross section
-  std::vector<GrpVal> nu_delayed_sigma_f;       ///< Delayed-Nubar-Sigmaf cross section
-  std::vector<GrpVal> inv_velocity;             ///< Groupwise inverse velocities
+  /// Energy bin boundaries in MeV
+  std::vector<std::array<double, 2>> e_bounds;
+
+  std::vector<double> sigma_t;  ///< Total cross section
+  std::vector<double> sigma_f;  ///< Fission cross section
+  std::vector<double> sigma_a;  ///< Absorption cross section
+
+  std::vector<double> chi;                      ///< Fission spectrum
+  std::vector<double> chi_prompt;               ///< Prompt fission spectrum
+  std::vector<std::vector<double>> chi_delayed; ///< Delayed emission spectra
+
+  std::vector<double> nu;         ///< Total neutrons per fission
+  std::vector<double> nu_prompt;  ///< Prompt neutrons per fission
+  std::vector<double> nu_delayed; ///< Delayed neutrons per fission
+
+  std::vector<double> nu_sigma_f;
+  std::vector<double> nu_prompt_sigma_f;
+  std::vector<double> nu_delayed_sigma_f;
+
+  std::vector<double> inv_velocity;
 
   std::vector<chi_math::SparseMatrix> transfer_matrices;
 
-  std::vector<PrecursorVal> precursor_lambda;         ///< Delayed neutron decay constants
-  std::vector<PrecursorVal> precursor_yield;          ///< Delayed neutron yields
-  std::vector<std::vector<PrecursorVal>> chi_delayed; ///< Delayed neutron fission spectrum
+  std::vector<double> precursor_lambda; ///< Precursor decay constants
+  std::vector<double> precursor_yield;  ///< Precursor yield fractions
 
   //Diffusion quantities
 public:
   bool diffusion_initialized = false;
 public:
-  std::vector<GrpVal> diffusion_coeff; ///< Transport corrected Diffusion coeff
-  std::vector<GrpVal> sigma_removal;   ///< Removal cross section
-  std::vector<GrpVal> sigma_s_gtog;    ///< Within-group scattering xs
+  std::vector<double> diffusion_coeff; ///< Transport corrected diffusion coeff
+  std::vector<double> sigma_removal;   ///< Removal cross section
+  std::vector<double> sigma_s_gtog;    ///< Within-group scattering xs
 
   //Monte-Carlo quantities
 public:
