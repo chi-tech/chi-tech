0f4a6ac036790bbd2a77de6374b90b0737f58a82
Jean Ragusa <jean.ragusa@tamu.edu>
2023-01-10 23:35:20 -0600

    updating tutorial doxy

--- doc/PAGES/Tutorials/Tutorial_00_Intro.h
+++ doc/PAGES/Tutorials/Tutorial_00_Intro.h
@@ -1,6 +1,25 @@
-/** \page Tutorial00 Tutorials
+/** \page Tutorial00 User Tutorials
+ * ## Fully commented tutorials
  *
  * \subpage Tutorial01 \n
  * \subpage Tutorial02 \n
  * \subpage Tutorial03 \n
+ *
+ ## Examples taken from the regression-test suite
+
+ Examples from the regression-test suite (folder ``tests``) may also serve as
+ additional tutorials in order for users to learn about additional possibilities
+ offered by chi-tech and its physics modules.
+
+ However, remember that those are regression tests that are meant to run fast.
+ Hence, you may want to revise the phase-space resolution
+ (e.g., mesh, angle, energy)and the iteration parameters (tolerance, number of
+ iterations) in your own input files in order to reach a satisfactory answer for
+ modeling purposes.
+
+ \e Note: The selection below does not include all regression tests.
+
+ * \subpage TutorialRegression \n
+<!-- Hide \include "../../ChiTest/Adjoint2D_2a_forward.lua" -->
+
  */
