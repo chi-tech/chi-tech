0d48b4d3afbc044a6f6bcdbfbcfd0b6db797e6da
JanMacM1 <janicvermaak@gmail.com>
2023-01-11 11:02:25 -0700

    Other modules quick ref

--- modules/module_lua_register.h
+++
@@ -1,4 +0,0 @@
-#include "../../modules/DiffusionSolver/lua/lua_register.h"
-#include "../../modules/LinearBoltzmannSolver/lua/lua_register.h"
-#include "../../modules/LBSCurvilinear/lua/lua_register.h"
-#include "../../modules/LBKEigenvalueSolver/lua/lua_register.h"
