0d4cb3a9d7b15acac113c49344860197cdcf943e
Jean Ragusa <jean.ragusa@tamu.edu>
2023-01-12 21:48:16 -0600

    fixing TG update

--- modules/MGDiffusion/mgd_02d_update_flux_with_two_grid_flux.cc
+++ modules/MGDiffusion/mgd_02d_update_flux_with_two_grid_flux.cc
@@ -8,30 +8,36 @@ void mg_diffusion::Solver::Update_Flux_With_TwoGrid(const int64_t verbose)
   const auto& grid = *grid_ptr;
   const auto& sdm  = *sdm_ptr;
 
+  // contains two_grid flux, stored in last num_groups entry
   const double *xlocal_tg;
   VecGetArrayRead(x[num_groups], &xlocal_tg);
 
   int counter = 0;
   for (const auto& cell : grid.local_cells)
   {
-    const double *xlocal;
-
     const auto& cell_mapping = sdm.GetCellMapping(cell);
     const size_t num_nodes = cell_mapping.NumNodes();
     const auto& xstg = map_mat_id_2_tginfo.at(cell.material_id);
 
     for (unsigned int g = last_fast_group; g < num_groups; ++g)
     {
-      VecGetArrayRead(x[g], &xlocal);
       for (size_t i=0; i<num_nodes; ++i)
       {
-        const int64_t imap = sdm.MapDOFLocal(cell, i); //MapDOFLocal ??
-        xlocal[imap] += xlocal_tg[imap] * VF[counter][i] * xstg.spectrum[g];
+        const int64_t imap       = sdm.MapDOFLocal(cell, i);
+        const int64_t imap_globl = sdm.MapDOF(cell, i);
+        const double aux = xlocal_tg[imap] * VF[counter][i] * xstg.spectrum[g];
+        VecSetValue(x[g], imap_globl, aux, ADD_VALUES);
       }// i
-      VecRestoreArrayRead(x[g], &xlocal);
     }//g
     counter++;
   }//for cell
 
+  // finalize
+  for (unsigned int g = last_fast_group; g < num_groups; ++g)
+  {
+    VecAssemblyBegin(x[g]);
+    VecAssemblyEnd(x[g]);
+  }
+  // release two_grid flux
   VecRestoreArrayRead(x[num_groups], &xlocal_tg);
 }
