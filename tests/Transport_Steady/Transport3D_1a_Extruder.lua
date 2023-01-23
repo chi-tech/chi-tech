de388d69dc4a0ad93d6d04c8acf28f54463ea76c
Jean Ragusa <jean.ragusa@tamu.edu>
2023-01-23 10:27:53 -0600

    reorganizing the tests folder

---
+++ tests/Transport_Steady/Transport3D_1a_Extruder.lua
@@ -0,0 +1,175 @@
+-- 3D Transport test with Vacuum and Incident-isotropic BC.
+-- SDM: PWLD
+-- Test: Max-value=5.27450e-01 and 3.76339e-04
+num_procs = 4
+
+
+
+
+
+--############################################### Check num_procs
+if (check_num_procs==nil and chi_number_of_processes ~= num_procs) then
+    chiLog(LOG_0ERROR,"Incorrect amount of processors. " ..
+                      "Expected "..tostring(num_procs)..
+                      ". Pass check_num_procs=false to override if possible.")
+    os.exit(false)
+end
+
+--############################################### Setup mesh
+chiMeshHandlerCreate()
+
+unpart_mesh = chiUnpartitionedMeshFromWavefrontOBJ(
+        "resources/TestMeshes/SquareMesh2x2Quads.obj",true)
+
+chiSurfaceMesherCreate(SURFACEMESHER_PREDEFINED);
+chiVolumeMesherCreate(VOLUMEMESHER_EXTRUDER,
+                      ExtruderTemplateType.UNPARTITIONED_MESH,
+                      unpart_mesh);
+
+NZ=2
+chiVolumeMesherSetProperty(EXTRUSION_LAYER,0.2*NZ,NZ,"Charlie");--0.4
+chiVolumeMesherSetProperty(EXTRUSION_LAYER,0.2*NZ,NZ,"Chuck");--0.8
+chiVolumeMesherSetProperty(EXTRUSION_LAYER,0.2*NZ,NZ,"Bob");--1.2
+chiVolumeMesherSetProperty(EXTRUSION_LAYER,0.2*NZ,NZ,"SarahConner");--1.6
+
+chiVolumeMesherSetProperty(PARTITION_TYPE,KBA_STYLE_XYZ)
+chiVolumeMesherSetKBAPartitioningPxPyPz(2,2,1)
+chiVolumeMesherSetKBACutsX({0.0})
+chiVolumeMesherSetKBACutsY({0.0})
+
+chiSurfaceMesherExecute();
+chiVolumeMesherExecute();
+
+--############################################### Set Material IDs
+vol0 = chiLogicalVolumeCreate(RPP,-1000,1000,-1000,1000,-1000,1000)
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,0)
+
+vol1 = chiLogicalVolumeCreate(RPP,-0.5,0.5,-0.5,0.5,-1000,1000)
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol1,1)
+
+--############################################### Add materials
+materials = {}
+materials[1] = chiPhysicsAddMaterial("Test Material");
+materials[2] = chiPhysicsAddMaterial("Test Material2");
+
+chiPhysicsMaterialAddProperty(materials[1],TRANSPORT_XSECTIONS)
+chiPhysicsMaterialAddProperty(materials[2],TRANSPORT_XSECTIONS)
+
+chiPhysicsMaterialAddProperty(materials[1],ISOTROPIC_MG_SOURCE)
+chiPhysicsMaterialAddProperty(materials[2],ISOTROPIC_MG_SOURCE)
+
+
+num_groups = 21
+chiPhysicsMaterialSetProperty(materials[1],TRANSPORT_XSECTIONS,
+        CHI_XSFILE,"tests/Transport_Steady/xs_graphite_pure.cxs")
+chiPhysicsMaterialSetProperty(materials[2],TRANSPORT_XSECTIONS,
+        CHI_XSFILE,"tests/Transport_Steady/xs_graphite_pure.cxs")
+
+src={}
+for g=1,num_groups do
+    src[g] = 0.0
+end
+
+chiPhysicsMaterialSetProperty(materials[1],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
+chiPhysicsMaterialSetProperty(materials[2],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
+
+--############################################### Setup Physics
+
+phys1 = chiLBSCreateSolver()
+
+--========== Groups
+grp = {}
+for g=1,num_groups do
+    grp[g] = chiLBSCreateGroup(phys1)
+end
+
+--========== ProdQuad
+pquad = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV,2, 2)
+pquad2 = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV,5, 5)
+
+--========== Groupset def
+gs0 = chiLBSCreateGroupset(phys1)
+cur_gs = gs0
+chiLBSGroupsetAddGroups(phys1,cur_gs,0,20)
+chiLBSGroupsetSetQuadrature(phys1,cur_gs,pquad)
+--chiLBSGroupsetSetAngleAggregationType(phys1,cur_gs,LBSGroupset.ANGLE_AGG_SINGLE)
+chiLBSGroupsetSetAngleAggDiv(phys1,cur_gs,1)
+chiLBSGroupsetSetGroupSubsets(phys1,cur_gs,1)
+chiLBSGroupsetSetIterativeMethod(phys1,cur_gs,NPT_GMRES)
+chiLBSGroupsetSetResidualTolerance(phys1,cur_gs,1.0e-6)
+if (master_export == nil) then
+    --chiLBSGroupsetSetEnableSweepLog(phys1,cur_gs,true)
+end
+chiLBSGroupsetSetMaxIterations(phys1,cur_gs,300)
+chiLBSGroupsetSetGMRESRestartIntvl(phys1,cur_gs,100)
+
+--############################################### Set boundary conditions
+bsrc={}
+for g=1,num_groups do
+    bsrc[g] = 0.0
+end
+bsrc[1] = 1.0/4.0/math.pi;
+chiLBSSetProperty(phys1,BOUNDARY_CONDITION,ZMIN,LBSBoundaryTypes.INCIDENT_ISOTROPIC,bsrc);
+
+chiLBSSetProperty(phys1,DISCRETIZATION_METHOD,PWLD)
+
+--############################################### Initialize and Execute Solver
+chiSolverInitialize(phys1)
+chiSolverExecute(phys1)
+
+--############################################### Get field functions
+fflist,count = chiLBSGetScalarFieldFunctionList(phys1)
+
+--############################################### Slice plot
+--slices = {}
+--for k=1,count do
+--    slices[k] = chiFFInterpolationCreate(SLICE)
+--    chiFFInterpolationSetProperty(slices[k],SLICE_POINT,0.0,0.0,0.8001)
+--    chiFFInterpolationSetProperty(slices[k],ADD_FIELDFUNCTION,fflist[k])
+--    --chiFFInterpolationSetProperty(slices[k],SLICE_TANGENT,0.393,1.0-0.393,0)
+--    --chiFFInterpolationSetProperty(slices[k],SLICE_NORMAL,-(1.0-0.393),-0.393,0.0)
+--    --chiFFInterpolationSetProperty(slices[k],SLICE_BINORM,0.0,0.0,1.0)
+--    chiFFInterpolationInitialize(slices[k])
+--    chiFFInterpolationExecute(slices[k])
+--    chiFFInterpolationExportPython(slices[k])
+--end
+
+--############################################### Volume integrations
+ffi1 = chiFFInterpolationCreate(VOLUME)
+curffi = ffi1
+chiFFInterpolationSetProperty(curffi,OPERATION,OP_MAX)
+chiFFInterpolationSetProperty(curffi,LOGICAL_VOLUME,vol0)
+chiFFInterpolationSetProperty(curffi,ADD_FIELDFUNCTION,fflist[1])
+
+chiFFInterpolationInitialize(curffi)
+chiFFInterpolationExecute(curffi)
+maxval = chiFFInterpolationGetValue(curffi)
+
+chiLog(LOG_0,string.format("Max-value1=%.5e", maxval))
+
+ffi1 = chiFFInterpolationCreate(VOLUME)
+curffi = ffi1
+chiFFInterpolationSetProperty(curffi,OPERATION,OP_MAX)
+chiFFInterpolationSetProperty(curffi,LOGICAL_VOLUME,vol0)
+chiFFInterpolationSetProperty(curffi,ADD_FIELDFUNCTION,fflist[20])
+
+chiFFInterpolationInitialize(curffi)
+chiFFInterpolationExecute(curffi)
+maxval = chiFFInterpolationGetValue(curffi)
+
+chiLog(LOG_0,string.format("Max-value2=%.5e", maxval))
+
+--############################################### Exports
+if (master_export == nil) then
+    chiExportFieldFunctionToVTKG(fflist[1],"ZPhi3D","Phi")
+end
+
+--############################################### Plots
+if (chi_location_id == 0 and master_export == nil) then
+
+    --os.execute("python ZPFFI00.py")
+    ----os.execute("python ZPFFI11.py")
+    --local handle = io.popen("python ZPFFI00.py")
+    print("Execution completed")
+end
+
---
+++ tests/Transport_Transient/TransientTransport2D_3.lua
@@ -0,0 +1,174 @@
+-- 1D Transient Transport test with Vacuum BC.
+-- SDM: PWLD
+-- Test:
+num_procs = 2
+
+
+
+
+
+--############################################### Check num_procs
+if (check_num_procs==nil and chi_number_of_processes ~= num_procs) then
+    chiLog(LOG_0ERROR,"Incorrect amount of processors. " ..
+            "Expected "..tostring(num_procs)..
+            ". Pass check_num_procs=false to override if possible.")
+    os.exit(false)
+end
+
+--############################################### Setup mesh
+chiMeshHandlerCreate()
+
+mesh={}
+N=160
+L=80.96897163
+xmin = -L/2
+dx = L/N
+for i=1,(N+1) do
+    k=i-1
+    mesh[i] = xmin + k*dx
+end
+chiMeshCreateUnpartitioned2DOrthoMesh(mesh,mesh)
+chiVolumeMesherExecute();
+
+--############################################### Set Material IDs
+chiVolumeMesherSetMatIDToAll(0)
+
+vol0 = chiLogicalVolumeCreate(RPP,-L/16,L/16,-L/16,L/16,-L/16,L/16)
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,1)
+
+chiMeshHandlerExportMeshToVTK("TheMesh")
+
+--############################################### Add materials
+materials = {}
+materials[1] = chiPhysicsAddMaterial("Strong fuel");
+materials[2] = chiPhysicsAddMaterial("Weak fuel");
+
+chiPhysicsMaterialAddProperty(materials[1],TRANSPORT_XSECTIONS)
+chiPhysicsMaterialAddProperty(materials[2],TRANSPORT_XSECTIONS)
+chiPhysicsMaterialAddProperty(materials[1],ISOTROPIC_MG_SOURCE)
+chiPhysicsMaterialAddProperty(materials[2],ISOTROPIC_MG_SOURCE)
+
+-- Define microscopic cross sections
+xs_strong_fuel_micro = chiPhysicsTransportXSCreate()
+chiPhysicsTransportXSSet(xs_strong_fuel_micro, CHI_XSFILE, "tests/Transport_Transient/xs_inf_k1_6_1g.cxs")
+xs_weak_fuelA_micro = chiPhysicsTransportXSCreate()
+chiPhysicsTransportXSSet(xs_weak_fuelA_micro, CHI_XSFILE, "tests/Transport_Transient/xs_inf_critical_1g.cxs")
+xs_weak_fuelB_micro = chiPhysicsTransportXSCreate()
+chiPhysicsTransportXSSet(xs_weak_fuelB_micro, CHI_XSFILE, "tests/Transport_Transient/xs_inf_weak2_1g.cxs")
+
+atom_density = 0.056559
+xs_strong_fuel = chiPhysicsTransportXSMakeCombined({{xs_strong_fuel_micro, atom_density}}) --critical
+xs_weak_fuelA  = chiPhysicsTransportXSMakeCombined({{ xs_weak_fuelA_micro, atom_density}}) --critical
+xs_weak_fuelB  = chiPhysicsTransportXSMakeCombined({{xs_weak_fuelB_micro, atom_density}})   --critical
+
+num_groups = 1
+chiPhysicsMaterialSetProperty(materials[1],TRANSPORT_XSECTIONS,
+        EXISTING,xs_strong_fuel)
+chiPhysicsMaterialSetProperty(materials[2],TRANSPORT_XSECTIONS,
+        EXISTING,xs_weak_fuelA)
+
+src={0.0}
+chiPhysicsMaterialSetProperty(materials[1],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
+chiPhysicsMaterialSetProperty(materials[2],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
+
+function SwapXS(solver_handle, new_xs)
+    chiPhysicsMaterialSetProperty(materials[2],TRANSPORT_XSECTIONS,
+            EXISTING,new_xs)
+    chiLBSInitializeMaterials(solver_handle)
+end
+
+--############################################### Setup Physics
+phys1 = chiLBSCreateTransientSolver()
+
+--========== Groups
+grp = {}
+for g=1,num_groups do
+    grp[g] = chiLBSCreateGroup(phys1)
+end
+
+--========== ProdQuad
+fac=3
+pquad = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV,2*fac, 2*fac)
+chiOptimizeAngularQuadratureForPolarSymmetry(pqaud, 4.0*math.pi)
+
+--========== Groupset def
+gs0 = chiLBSCreateGroupset(phys1)
+cur_gs = gs0
+chiLBSGroupsetAddGroups(phys1,cur_gs,0,num_groups-1)
+chiLBSGroupsetSetQuadrature(phys1,cur_gs,pquad)
+chiLBSGroupsetSetAngleAggDiv(phys1,cur_gs,1)
+chiLBSGroupsetSetGroupSubsets(phys1,cur_gs,1)
+chiLBSGroupsetSetIterativeMethod(phys1,cur_gs,KRYLOV_GMRES_CYCLES)
+chiLBSGroupsetSetResidualTolerance(phys1,cur_gs,1.0e-6)
+chiLBSGroupsetSetMaxIterations(phys1,cur_gs,1000)
+chiLBSGroupsetSetGMRESRestartIntvl(phys1,cur_gs,100)
+--chiLBSGroupsetSetWGDSA(phys1,cur_gs,30,1.0e-4,false," ")
+--chiLBSGroupsetSetTGDSA(phys1,cur_gs,30,1.0e-4,false," ")
+
+--
+----############################################### Set boundary conditions
+--bsrc={}
+--for g=1,num_groups do
+--    bsrc[g] = 0.0
+--end
+--bsrc[1] = 1.0/2
+--chiLBSSetProperty(phys1,BOUNDARY_CONDITION,ZMIN,LBSBoundaryTypes.REFLECTING);
+--chiLBSSetProperty(phys1,BOUNDARY_CONDITION,ZMAX,LBSBoundaryTypes.REFLECTING);
+--
+chiLBSSetProperty(phys1,DISCRETIZATION_METHOD,PWLD)
+chiLBSSetProperty(phys1,SCATTERING_ORDER,0)
+
+chiLBKESSetProperty(phys1, "MAX_ITERATIONS", 1000)
+chiLBKESSetProperty(phys1, "TOLERANCE", 1.0e-8)
+
+chiLBSSetProperty(phys1, USE_PRECURSORS, true)
+
+--chiLBSSetProperty(phys1, VERBOSE_INNER_ITERATIONS, false)
+chiLBSSetProperty(phys1, VERBOSE_INNER_ITERATIONS, false)
+chiLBSSetProperty(phys1, VERBOSE_OUTER_ITERATIONS, true)
+
+
+--############################################### Initialize and Execute Solver
+chiSolverInitialize(phys1)
+
+chiLBTSSetProperty(phys1, "TIMESTEP", 1e-3)
+chiLBTSSetProperty(phys1, "VERBOSITY_LEVEL", 0)
+chiLBTSSetProperty(phys1, "TIMESTEP_METHOD", "CRANK_NICHOLSON")
+
+phys1name = chiSolverGetName(phys1);
+initial_FR = chiLBSComputeFissionRate(phys1,"OLD")
+
+--time = 0.0
+--for k=1,2 do
+--    --chiLBTSSetProperty(phys1, "INHIBIT_ADVANCE", true)
+--    chiSolverStep(phys1)
+--    FRf = chiLBSComputeFissionRate(phys1,"NEW")
+--    FRi = chiLBSComputeFissionRate(phys1,"OLD")
+--    dt = chiLBTSGetProperty(phys1, "TIMESTEP")
+--    time = chiLBTSGetProperty(phys1, "TIME")
+--    period = dt/math.log(FRf/FRi)
+--    chiLog(LOG_0, string.format("%s %4d time=%10.3g dt=%10.4g period=%10.3g FR=%10.3e",
+--            phys1name,k,time,dt,period,FRf/initial_FR))
+--end
+
+time = 0.0
+time_stop = 1.0
+k=0
+swapped = false
+while (time < time_stop) do
+    k = k + 1
+    chiSolverStep(phys1)
+    FRf = chiLBSComputeFissionRate(phys1,"NEW")
+    FRi = chiLBSComputeFissionRate(phys1,"OLD")
+    dt = chiLBTSGetProperty(phys1, "TIMESTEP")
+    time = chiLBTSGetProperty(phys1, "TIME")
+    period = dt/math.log(FRf/FRi)
+    chiLog(LOG_0, string.format("%s %4d time=%10.3g dt=%10.4g period=%10.3g FR=%10.3e",
+            phys1name,k,time,dt,period,FRf/initial_FR))
+    if (time >= 0.2 and not swapped) then
+        SwapXS(phys1, xs_weak_fuelB)
+        swapped = true
+    end
+
+    chiLBTSAdvanceTimeData(phys1)
+end
