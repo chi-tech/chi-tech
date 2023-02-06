25b107b9fe7250f9e8b1592df6dc82a8b034ba0b
DrJan <janizak.vermaak@inl.gov>
2023-02-06 14:53:58 -0700

    Fix for big test QBlock

--- tests/BigTests/QBlock/materials.lua
+++ tests/BigTests/QBlock/materials.lua
@@ -5,8 +5,8 @@ for m=0,1 do
     xs[tostring(m)] = chiPhysicsTransportXSCreate()
 end
 
-chiPhysicsTransportXSSet(xs["0"],CHI_XSFILE,"xs_water_g2.cxs")
-chiPhysicsTransportXSSet(xs["1"],CHI_XSFILE,"xs_fuel_g2.cxs")
+chiPhysicsTransportXSSet(xs["0"],CHI_XSFILE,"xs_water_g2_bak.cxs")
+chiPhysicsTransportXSSet(xs["1"],CHI_XSFILE,"xs_fuel_g2_bak.cxs")
 
 water_xs = chiPhysicsTransportXSGet(xs["0"])
 num_groups = water_xs["num_groups"]
