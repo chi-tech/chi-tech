f57b14b02d764ce080e2e0ae237965ca6b539862
DrJan <janizak.vermaak@inl.gov>
2023-03-01 13:23:50 -0700

    First working version of exodus exporter

--- framework/ChiMesh/MeshHandler/lua/meshhandler_lua.h
+++ framework/ChiMesh/MeshHandler/lua/meshhandler_lua.h
@@ -7,5 +7,6 @@ int chiMeshHandlerCreate(lua_State *L);
 int chiMeshHandlerSetCurrent(lua_State *L);
 int chiMeshHandlerExportMeshToObj(lua_State* L);
 int chiMeshHandlerExportMeshToVTK(lua_State* L);
+int chiMeshHandlerExportMeshToExodus(lua_State* L);
 
 #endif //CHITECH_MESHHANDLER_LUA_H
