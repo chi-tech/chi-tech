5911f4520411dffa31388e61ebd3b46e0fd9cef2
DrJan <janizak.vermaak@inl.gov>
2023-03-08 13:14:59 -0700

    Another adjustment

--- modules/LinearBoltzmannSolvers/A_LBSSolver/IterativeMethods/ags_linear_solver.cc
+++ modules/LinearBoltzmannSolvers/A_LBSSolver/IterativeMethods/ags_linear_solver.cc
@@ -86,12 +86,13 @@ void AGSLinearSolver<Mat,Vec,KSP>::Solve()
   Vec x_old;
   VecDuplicate(x_, &x_old);
 
+  //Save qmoms to be restored after each iteration.
+  //This is necessary for multiple ags iterations to function
+  //and for keigen-value problems
+  const auto saved_qmoms = lbs_solver.QMomentsLocal();
+
   for (int iter = 0; iter < tolerance_options_.maximum_iterations; ++iter)
   {
-    //Save qmoms to be restored after the iteration.
-    //This is necessary for multiple ags iterations to function
-    //and for keigen-value problems
-    const auto saved_qmoms = lbs_solver.QMomentsLocal();
 
     lbs_solver.SetGroupScopedPETScVecFromPrimarySTLvector(gid_i,gid_f,x_old,phi);
 
@@ -114,11 +115,12 @@ void AGSLinearSolver<Mat,Vec,KSP>::Solve()
       << " Relative change " << std::setw(10) << std::setprecision(4)
       << error_norm/sol_norm;
 
+    lbs_solver.QMomentsLocal() = saved_qmoms; //Restore qmoms
+
     if (error_norm < tolerance_options_.residual_absolute)
       break;
-
-    lbs_solver.QMomentsLocal() = saved_qmoms; //Restore qmoms
   }//for iteration
+
 
   VecDestroy(&x_old);
 }
