6a7f0d4a6c12449f24b81197a99cd89c2e177d81
DrJan <janizak.vermaak@inl.gov>
2023-08-29 09:43:27 -0600

    Quick fix for ParallelVector test failing

--- test/framework/chi_math/chi_math_test_02.cc
+++ test/framework/chi_math/chi_math_test_02.cc
@@ -6,53 +6,42 @@
 
 #include "console/chi_console.h"
 
-#include <unistd.h>
-
-
 namespace chi_unit_tests
 {
 
 chi::ParameterBlock
-chi_math_Test02(const chi::InputParameters& params);
+chi_math_Test02_ParallelVector(const chi::InputParameters& params);
 
 RegisterWrapperFunction(/*namespace_name=*/chi_unit_tests,
-                        /*name_in_lua=*/chi_math_Test02,
+                        /*name_in_lua=*/chi_math_Test02_ParallelVector,
                         /*syntax_function=*/nullptr,
-                        /*actual_function=*/chi_math_Test02);
+                        /*actual_function=*/chi_math_Test02_ParallelVector);
 
 chi::ParameterBlock
-chi_math_Test02(const chi::InputParameters& params)
+chi_math_Test02_ParallelVector(const chi::InputParameters& params)
 {
   using namespace chi_math;
 
-  Chi::log.Log() << "GOLD_BEGIN";
+  ChiLogicalErrorIf(Chi::mpi.process_count != 2, "Requires 2 processors");
 
   Chi::log.Log() << "Testing chi_math::ParallelVector" << std::endl;
 
   ParallelVector vec(5, 10, Chi::mpi.comm);
 
-  if (Chi::mpi.location_id == 0)
-    vec.SetValue(5, 2.0, VecOpType::SET_VALUE);
+  if (Chi::mpi.location_id == 0) vec.SetValue(5, 2.0, VecOpType::SET_VALUE);
   else
     vec.SetValue(0, 1.0, VecOpType::SET_VALUE);
   vec.Assemble();
 
-  Chi::mpi.Barrier();
-  Chi::log.LogAll() << vec.PrintStr() << std::endl;
-  Chi::mpi.Barrier();
-  usleep(1'000);
+  Chi::log.LogAll() << "vec after assembly: " << vec.PrintStr() << std::endl;
 
-  Chi::log.Log()
-      << "Testing chi_math::GhostedParallelVector" << std::endl;
+  Chi::log.Log() << "Testing chi_math::GhostedParallelVector" << std::endl;
 
   const int64_t ghost_id = Chi::mpi.location_id == 0 ? 5 : 4;
   GhostedParallelVector ghost_vec(5, 10, {ghost_id}, Chi::mpi.comm);
 
-  Chi::mpi.Barrier();
-  Chi::log.LogAll()
-      << "Number of Ghosts: " << ghost_vec.NumGhosts() << std::endl;
-  Chi::mpi.Barrier();
-  usleep(1'000);
+  Chi::log.LogAll() << "Number of Ghosts: " << ghost_vec.NumGhosts()
+                    << std::endl;
 
   if (Chi::mpi.location_id == 0)
     ghost_vec.SetValue(5, 2.0, VecOpType::SET_VALUE);
@@ -61,13 +50,10 @@ chi_math_Test02(const chi::InputParameters& params)
   ghost_vec.Assemble();
   ghost_vec.CommunicateGhostEntries();
 
-  Chi::mpi.Barrier();
-  Chi::log.LogAll() << ghost_vec.PrintStr() << std::endl;
-  Chi::mpi.Barrier();
-  usleep(1'000);
+  Chi::log.LogAll() << "Ghost vec after communicate: " << ghost_vec.PrintStr()
+                    << std::endl;
 
-  Chi::log.Log() << "GOLD_END";
   return chi::ParameterBlock();
 }
 
-}
+} // namespace chi_unit_tests
