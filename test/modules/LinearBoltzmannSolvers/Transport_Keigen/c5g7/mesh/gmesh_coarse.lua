cea7e9727f5ca2dda83899ef44b1bb3f2bd8f027
DrJan <janizak.vermaak@inl.gov>
2023-09-05 13:03:22 -0600

    Review comments

--- test/modules/LinearBoltzmannSolvers/Transport_Keigen/c5g7/mesh/gmesh_coarse.lua
+++ test/modules/LinearBoltzmannSolvers/Transport_Keigen/c5g7/mesh/gmesh_coarse.lua
@@ -7,12 +7,6 @@ umesh = chiUnpartitionedMeshFromMshFormat("mesh/2D_c5g7_coarse.msh",true)
 chiSurfaceMesherCreate(SURFACEMESHER_PREDEFINED);
 chiVolumeMesherCreate(VOLUMEMESHER_UNPARTITIONED,umesh);
 
---chiVolumeMesherSetKBAPartitioningPxPyPz(2,2,1)
---chiVolumeMesherSetKBACutsX({27.72})
---chiVolumeMesherSetKBACutsY({27.72})
---chiVolumeMesherSetProperty(PARTITION_TYPE,KBA_STYLE_XYZ)
-
-
 --############################################### Execute meshing
 chiSurfaceMesherExecute();
 chiVolumeMesherExecute();
