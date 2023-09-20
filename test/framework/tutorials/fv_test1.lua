e746c3871ceaa02caf44f545ef2cd67237ed4e4b
ragusa <jean.ragusa@tamu.edu>
2023-09-19 22:36:40 -0500

    new syntax, just layout

--- test/framework/tutorials/fv_test1.lua
+++ test/framework/tutorials/fv_test1.lua
@@ -1,7 +1,7 @@
 --############################################### Setup mesh
 nodes={}
 N=100
-L=2.0
+L=2
 xmin = -L/2
 dx = L/N
 for i=1,(N+1) do
@@ -9,7 +9,10 @@ for i=1,(N+1) do
     nodes[i] = xmin + k*dx
 end
 
-meshgen1 = chi_mesh.OrthogonalMeshGenerator.Create({ node_sets = {nodes,nodes} })
+meshgen1 = chi_mesh.OrthogonalMeshGenerator.Create
+({
+  node_sets = {nodes,nodes}
+})
 chi_mesh.MeshGenerator.Execute(meshgen1)
 
 --############################################### Set Material IDs
