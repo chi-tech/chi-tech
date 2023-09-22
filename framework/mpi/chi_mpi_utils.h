def1d95ee29909050492d3943f1e3208ef3420b2
DrJan <janizak.vermaak@inl.gov>
2023-09-22 09:18:18 -0600

    MPI Utiltities
    Given a communicator we seem to have a lot of code duplicated
      when getting the current rank off, number of total ranks, and figuring out
      the block offsets for DOFs. Added three utilities for this.

--- framework/mpi/chi_mpi_utils.h
+++ framework/mpi/chi_mpi_utils.h
@@ -5,7 +5,15 @@
 
 namespace chi_mpi_utils
 {
+/**Returns the current rank on the specified communicator.*/
+int GetLocationID(MPI_Comm mpi_comm);
+/**Returns the total number of ranks on the specified communicator.*/
+int GetProcessCount(MPI_Comm mpi_comm);
 
+/**Given each location's local size (of items), builds a vector (dimension
+* comm-size plus 1) of where each location's global indices start and end.
+* Example: location i starts at extents[i] and ends at extents[i+1]*/
+std::vector<uint64_t> BuildLocationExtents(uint64_t local_size, MPI_Comm comm);
 }//namespace chi_mpi_utils
 
 #endif //CHITECH_CHI_MPI_UTILS_H
